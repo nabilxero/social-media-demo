fd80ce0fb26f34d44a74c1eaa6dfee5f4b03857d
Nabil <nurahasnatnabil@gmail.com>
2021-07-02 19:45:39 +0600

    initial commit

---
+++ src/test/java/com/bs23test/socialmediademo/SocialMediaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs23test.socialmediademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
